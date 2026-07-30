ab90bcd4af4456e100c5ab46a741e8dc61604573
Pavel <pavel.vorobev.al@gmail.com>
2026-07-30 18:58:49 +0200

    Add logging in route

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, HTTPException, Request
 from app.models.seed_model import SeedInputModel, PredictionResponse
 from app.services.predictor import predict_seed
+import logging
 
 router = APIRouter()
 
+logger = logging.getLogger(__name__)
+
 @router.post("/predict", response_model=PredictionResponse)
 async def seed_entry(data: SeedInputModel, request: Request):
+
+    logger.info("Prediction request received.")
 
     try:
 
@@ -15,12 +20,15 @@ async def seed_entry(data: SeedInputModel, request: Request):
             request.app.state.encoder
             )
 
+        logger.info(f"Prediction successful: {prediction}")
+
         return PredictionResponse(
             message="Data received",
             prediction=prediction
         )
 
-    except Exception:
+    except Exception as e:
+        logger.error(f"Prediction failed: {e}")
         raise HTTPException(
             status_code=500,
             detail="Prediction failed."
