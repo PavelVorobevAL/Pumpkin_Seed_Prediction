1b0a30ffd2463288066fca9e7bd7b224f9f78a8a
Pavel <pavel.vorobev.al@gmail.com>
2026-07-30 18:34:34 +0200

    Add HttpExcepton in predict router

--- app/models/seed_model.py
+++ app/models/seed_model.py
@@ -1,18 +1,18 @@
 from pydantic import BaseModel, Field
 
 class SeedInputModel(BaseModel):
-    Area: int = Field(gt=0)
+    Area: int = Field(gt=0, description="Seed area")
     Perimeter: float = Field(gt=0)
     Major_Axis_Length: float = Field(gt=0)
     Minor_Axis_Length: float = Field(gt=0)
     Convex_Area: int = Field(gt=0)
     Equiv_Diameter: float = Field(gt=0)
-    Eccentricity: float = Field(gt=0)
-    Solidity: float = Field(gt=0)
-    Extent: float = Field(gt=0)
-    Roundness: float = Field(gt=0)
+    Eccentricity: float = Field(gt=0, le=1)
+    Solidity: float = Field(gt=0, le=1)
+    Extent: float = Field(gt=0, le=1)
+    Roundness: float = Field(gt=0, le=1)
     Aspect_Ration: float = Field(gt=0)
-    Compactness: float = Field(gt=0)
+    Compactness: float = Field(gt=0, le=1)
 
 class PredictionResponse(BaseModel):
     message: str
--- app/routes/predict.py
+++ app/routes/predict.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Request
 from app.models.seed_model import SeedInputModel, PredictionResponse
-from fastapi import Request
 from app.services.predictor import predict_seed
 
 router = APIRouter()
@@ -8,9 +7,21 @@ router = APIRouter()
 @router.post("/predict", response_model=PredictionResponse)
 async def seed_entry(data: SeedInputModel, request: Request):
 
-    prediction = predict_seed(data, request.app.state.model, request.app.state.encoder)
+    try:
 
-    return PredictionResponse(
-        message="Data received",
-        prediction=prediction
-    )
+        prediction = predict_seed(
+            data, 
+            request.app.state.model, 
+            request.app.state.encoder
+            )
+
+        return PredictionResponse(
+            message="Data received",
+            prediction=prediction
+        )
+
+    except Exception:
+        raise HTTPException(
+            status_code=500,
+            detail="Prediction failed."
+        )
