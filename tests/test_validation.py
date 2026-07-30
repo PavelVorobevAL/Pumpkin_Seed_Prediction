babb69580018e457aac805c48ecc9e37ef072da4
Pavel <pavel.vorobev.al@gmail.com>
2026-07-30 20:43:34 +0200

    Add a validation test

---
+++ tests/test_validation.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient as TestClient
+from app.main import app
+
+def validation():
+    with TestClient(app) as client:
+
+        data = {
+            "Area": -100,
+            "Perimeter": 900,
+            "Major_Axis_Length": 350,
+            "Minor_Axis_Length": 180,
+            "Convex_Area": 50500,
+            "Equiv_Diameter": 250,
+            "Eccentricity": 0.8,
+            "Solidity": 0.99,
+            "Extent": 0.75,
+            "Roundness": 0.78,
+            "Aspect_Ration": 1.9,
+            "Compactness": 0.72
+        }
+
+        response = client.post("/predict", json=data)
+        assert response.status_code == 422
